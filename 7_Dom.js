a12dd6c70eb550178a81f5d802eaaba196c8562c
Mr_Rinku_kumar_saini <90628218+MrRinkukumar@users.noreply.github.com>
2022-02-21 22:49:19 +0530

    Create 7_Dom.js

---
+++ 7_Dom.js
@@ -0,0 +1,17 @@
+console.log(`here we study about dom`);
+let a =document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0];
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+// a = document.links[0].href;
+// a = document.images;
+// a = document.scripts;
+// Array.from(a).forEach(function(element){
+//   document.body.innerHTML =element;
+// })
+// let b= `rinku`;
+// document.body.innerHTML=b;
+console.log(a);
