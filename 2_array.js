51eceb283d62867445e771cd6da22eb6c09c2c23
Mr_Rinku_kumar_saini <90628218+MrRinkukumar@users.noreply.github.com>
2022-02-21 22:48:52 +0530

    Create 2_array.js

---
+++ 2_array.js
@@ -0,0 +1,36 @@
+let marks = [16,22,55,44,77,88,99];
+/*const fruit = [`orange `,`apple`,`pine apple`];
+const mixed =[`str`,66,88,[3,5]];
+const arr = new Array (44,55,566,`orange`);
+console.log(marks);
+console.log(fruit);
+console.log(mixed);
+console.log(arr);
+console.log(arr.length);
+console.log(Array.isArray(`rinku`,`kumar`));
+arr[0]= `rinku`;
+console.log(arr);
+let value = marks.indexOf(77);
+console.log(value);
+
+// muteting
+let marks2 = [1,2,3,4,5];
+// marks.push(1222);
+// marks.unshift(233);
+// marks.pop();
+// marks.shift();
+// marks.splice(5,2);
+// marks.reverse();
+marks=marks.concat(marks2);
+console.log(marks);
+*/
+let myobject={
+    name:'rinku',
+    student:'ele'
+}
+console.log(myobject);
+console.log(myobject.name);
+console.log(myobject[`student`]);
+
+
+
