16c34dec5ffd11b5945b00df35033b1b22993e4b
Mr_Rinku_kumar_saini <90628218+MrRinkukumar@users.noreply.github.com>
2022-02-21 22:48:15 +0530

    Create 1-java.js

---
+++ 1-java.js
@@ -0,0 +1,38 @@
+// console.time("your code time taken");
+console.log("rinku kumar saini");
+console.table({rinku: 166, arpit: 233, gajendra:333});
+console.warn("this is last warning");
+console.assert(33<44,"rinku");
+// console.timeEnd("your code time taken");
+const rinku="write the name ";
+const kumar ="here";
+// console.log(rinku+kumar);
+// some important funcation about a string
+/*rinku.concat(this,rinku);
+let html;
+html=" <h1> rinku kumar </h1>";
+html=html.concat('this');
+console.log(html.toLocaleLowerCase());
+console.log(html.toLocaleUpperCase());
+console.log(html[2]);
+console.log(html.indexOf("<"));
+console.log(html.lastIndexOf("<"));
+console.log(html.charAt(3));
+console.log(html.endsWith("this"));
+console.log(html.includes("ty"));
+console.log(html.substring(0,5));
+console.log(html.slice(0,4));
+console.log(html.slice(4));
+console.log(html.split(' '));
+console.log(html.replace('this','it'));*/
+//  some  use ful of bacstics 
+let fruit1 = `orand`;
+let fruit12 =` rikujkkf`;
+let myhtml = `hellow ${rinku} and ${kumar} ok`;
+document.body.innerHTML = myhtml;
+
+// array and hs properties
+
+
+
+
