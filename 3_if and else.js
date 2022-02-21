5ed33f99bb3ebb8569ac727a2326f1863decf3ec
Mr_Rinku_kumar_saini <90628218+MrRinkukumar@users.noreply.github.com>
2022-02-21 22:49:03 +0530

    Create 3_if and else.js

---
+++ 3_if and else.js
@@ -0,0 +1,41 @@
+console.log(`if else statement`);
+let rinku_age=20;
+let r = `rinku kumar saini`;
+/* this is if else statement basic in java script-----------------------------------------------
+if(rinku_age<20)
+{
+    console.log(` ${r} is under 20`);
+}
+else if(rinku_age===18)
+{
+    console.log(`${r} is18`)
+}
+else{
+    console.log(`${r} is above 20`);
+
+}*/
+
+/* this is a turnary operation in java script----------------------------------------------------
+console.log( rinku_age==20? `age is 20`:`you can drive`);
+console.log( here write condition ? ` condition is true`:`if condition is false`);
+
+*/
+/* this is basic statement for switch case in java script----------------------------------------
+
+switch (rinku_age) {
+    case 18: 
+    console.log(`${r} is 18 year old`)
+        break;
+        case 20:
+    console.log(`${r} is above 18 year old`)
+        break;
+        case 30:
+    console.log(`${r} is 50 year old`)
+        break;
+
+    default:
+        console.log(`you entered rong`);
+        break;
+}
+*/
+
